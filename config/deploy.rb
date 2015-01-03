dee7335b9645b79b5bae9709c2c05398d8ce5fd5
Sean Chitwood <darkmane@gmail.com>
2015-01-03 13:15:06 -0800

    Add html

---
+++ config/deploy.rb
@@ -0,0 +1,72 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+
+set :application, 'showcase'
+set :repo_url, 'git@showcase:darkmane/showcase.git'
+
+set :scm, :git
+
+set :local_user, "deploy"
+set :deploy_to, "/var/www/showcase"
+set :deploy_via, :copy
+set :copy_dir, "/var/tmp/cap_showcase"
+set :copy_remote_dir, "/var/tmp/cap_showcase"
+set :copy_cache, "#{fetch(:copy_dir)}/#{fetch(:application)}"
+set :copy_exclude, [".git", "**/.git"]
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+	task :migrate do
+    puts "    not doing migrate because not a Rails application."
+  end
+  task :finalize_update do
+    puts "    not doing finalize_update because not a Rails application."
+  end
+  task :start do
+    puts "    not doing start because not a Rails application."
+  end
+  task :stop do 
+    puts "    not doing stop because not a Rails application."
+  end
+  task :restart do
+    puts "    not doing restart because not a Rails application."
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
