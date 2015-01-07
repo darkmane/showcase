d7f438bb389cf0e438cf097b39ee9e76814c526a
Sean Chitwood <darkmane@gmail.com>
2015-01-06 20:25:11 -0800

    Add bootstrap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@ role :db,  %w{deploy@darkmane.darkmane.me}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'www.darkmane.me', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'darkmane.darkmane.me', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
