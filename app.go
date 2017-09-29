eeb24286f65928c1bc85d1e0d6da6109ab178767
Sean Chitwood <darkmane@gmail.com>
2017-09-29 12:41:30 -0700

    Migrate to using Cloud storage

--- app.go
+++ app.go
@@ -3,34 +3,23 @@ package hello
 import (
     "fmt"
     "net/http"
+    "bytes"
+    "io"
+    "io/ioutil"
+    //"strings"
+
+    "cloud.google.com/go/storage"
+    "golang.org/x/net/context"
+		//"google.golang.org/api/iterator"
+    "google.golang.org/appengine"
+    "google.golang.org/appengine/file"
+    "google.golang.org/appengine/log"
 )
 
 func init() {
-    http.HandleFunc("/.*", handler)
+    http.HandleFunc("/", handler)
 }
 
-// readFile reads the named file in Google Cloud Storage.
-func (d *demo) readFile(fileName string) {
-
-    rc, err := d.bucket.Object(fileName).NewReader(d.ctx)
-    if err != nil {
-        d.errorf("readFile: unable to open file from bucket %q, file %q: %v", d.bucketName, fileName, err)
-        return
-    }
-    defer rc.Close()
-    slurp, err := ioutil.ReadAll(rc)
-    if err != nil {
-        d.errorf("readFile: unable to read data from bucket %q, file %q: %v", d.bucketName, fileName, err)
-        return
-    }
-
-    fmt.Fprintf(d.w, "%s\n", bytes.SplitN(slurp, []byte("\n"), 2)[0])
-    if len(slurp) > 1024 {
-        fmt.Fprintf(d.w, "...%s\n", slurp[len(slurp)-1024:])
-    } else {
-        fmt.Fprintf(d.w, "%s\n", slurp)
-    }
-}
 
 // demo struct holds information needed to run the various demo functions.
 type demo struct {
@@ -55,7 +44,6 @@ func (d *demo) errorf(format string, args ...interface{}) {
 // handler is the main demo entry point that calls the GCS operations.
 func handler(w http.ResponseWriter, r *http.Request) {
     ctx := appengine.NewContext(r)
-
     //[START get_default_bucket]
     // Use `dev_appserver.py --default_gcs_bucket_name GCS_BUCKET_NAME`
     // when running locally.
@@ -81,8 +69,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
         bucketName: bucket,
     }
 
-    n := r.URL.Path
-    d.readFile(n)
+    path := r.URL.Path
+		host := r.URL.Host
+		if !r.URL.IsAbs() && host == "" {
+			host = "www.darkmane.me"
+		}
+		log.Infof(ctx, "Host: %s, %s", host, path)
+		d.readFile(fmt.Sprintf("%s%s", host, path))
 
     if d.failed {
         w.WriteHeader(http.StatusInternalServerError)
@@ -99,7 +92,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 //[START read]
 // readFile reads the named file in Google Cloud Storage.
 func (d *demo) readFile(fileName string) {
-    io.WriteString(d.w, "\nAbbreviated file content (first line and last 1K):\n")
+
 
     rc, err := d.bucket.Object(fileName).NewReader(d.ctx)
     if err != nil {
